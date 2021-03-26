d4a606363ba6895468fb4df20af4ec0eed4d59cc
vovkpavlik <vovk.pavlik@bk.ru>
2021-03-26 11:16:38 +0300

    Добавил редактирование поля new_building.

--- property/admin.py
+++ property/admin.py
@@ -8,5 +8,17 @@ class FlatAdmin(admin.ModelAdmin):
 
     readonly_fields = ["created_at"]
 
+    list_display = (
+    "address",
+    "price",
+    "new_building",
+    "construction_year",
+    "town",
+
+    )
+
+    list_editable = ["new_building"]
+
+
 
 admin.site.register(Flat, FlatAdmin)
