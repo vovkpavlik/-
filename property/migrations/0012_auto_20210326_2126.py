046643a9491bbaff52da75ed7fbd846756f4d883
vovkpavlik <vovk.pavlik@bk.ru>
2021-03-26 21:53:32 +0300

    Добавил проверку на ошибки нормализованного номера.

--- property/migrations/0012_auto_20210326_2126.py
+++ property/migrations/0012_auto_20210326_2126.py
@@ -5,8 +5,10 @@ import phonenumbers
 def owner_pure_phone(apps, schema_editor):
     Flat = apps.get_model("property", "Flat")
     for flat in Flat.objects.all():
-        flat.owner_pure_phone = phonenumbers.parse(flat.owners_phonenumber, "RU")
-        flat.save()
+        pure_number = phonenumbers.parse(flat.owners_phonenumber, "RU")
+        if phonenumbers.is_valid_number(pure_number):
+            flat.owner_pure_phone = pure_number
+            flat.save()
 
 
 class Migration(migrations.Migration):
